f4b47bb37941ce7756e9836c9e8c90a8624e6f07
Seliun Ignat <seliunignat@gmail.com>
2021-11-28 14:45:32 +0300

    Commit Init

---
+++ src/com/os/Main.java
@@ -0,0 +1,13 @@
+package com.os;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
